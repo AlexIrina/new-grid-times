01e327b260e5c42a734aebdfcdc95f8028c77680
Alex George <alexgeorge2863@gmail.com>
2021-12-08 17:12:59 -0700

    add the opinion-stories section on tablet and up. Add a border right and a border bottom. Align opinion-stories column up 8px on tablet and up

--- src/components/MainStoryGrid/MainStoryGrid.js
+++ src/components/MainStoryGrid/MainStoryGrid.js
@@ -63,6 +63,15 @@ const Wrapper = styled.div`
 		grid-template-columns: 2fr 1fr;
 		gap: 48px 0;
 	}
+
+	/*TODO: Turn to 3 columns on laptop */
+	@media ${QUERIES.laptopAndUp} {
+		grid-template-areas:
+			'main-story secondary-stories opinion-stories'
+			'main-story advertisement advertisement';
+		grid-template-columns: 5fr 4fr 3fr;
+		gap: 0;
+	}
 `
 
 const MainStorySection = styled.section`
@@ -77,6 +86,12 @@ const MainStorySection = styled.section`
 
 const SecondaryStorySection = styled.section`
 	grid-area: secondary-stories;
+
+	@media ${QUERIES.laptopAndUp} {
+		margin-right: 16px;
+		padding-right: 16px;
+		border-right: 1px solid var(--color-gray-300);
+	}
 `
 
 const VerticalStoryWrapper = styled.div`
@@ -111,10 +126,18 @@ const OpinionStoryList = styled(StoryList)`
 
 const OpinionSection = styled.section`
 	grid-area: opinion-stories;
+	@media ${QUERIES.laptopAndUp} {
+		margin-top: -8px;
+	}
 `
 
 const AdvertisementSection = styled.section`
 	grid-area: advertisement;
+	@media ${QUERIES.laptopAndUp} {
+		margin-top: 16px;
+		padding-top: 16px;
+		border-top: 1px solid var(--color-gray-300);
+	}
 `
 
 export default MainStoryGrid
